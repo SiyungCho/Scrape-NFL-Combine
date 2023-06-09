3291047c8a3e66a7abf3312757a5590ee7dce700
Siyung <siyungcho@gmail.com>
2023-06-09 12:35:06 -0400

    headers split

--- Src/Scrapper.py
+++ Src/Scrapper.py
@@ -3,12 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-from bs4 import BeautifulSoup
-import requests
-
 import pandas as pd
-
-import player
 
 path = Service("C:\\Users\\siyun\\Downloads\\chromedriver_win32\\chromedriver.exe")
 NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?'
@@ -16,17 +11,20 @@ NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?'
 driver = webdriver.Chrome(service=path)
 driver.get(NFL_url)
 
-data = driver.find_elements(By.TAG_NAME, "tbody")
 headers = driver.find_elements(By.TAG_NAME, "thead")
-
 for element in headers:
     text = element.text
-    #print(text)
 
-print(headers[0][0].text)
+col_names = text.split("\n")
+col_names = col_names[:-1] + col_names[-1].split(' ')
+col_names.remove('Wonderlic')
+print(col_names)
 
+data = driver.find_elements(By.TAG_NAME, "tbody")
 for element in data:
     text = element.text
-    #print(text)
+
+all_values = text.split("\n")
+print(all_values)
 
 driver.quit()
