836efc711a51500c505a657fb26df4e8e0704492
Siyung <siyungcho@gmail.com>
2023-06-28 00:01:58 -0400

    got every year data

--- Src/all_players_scrapper.py
+++ Src/all_players_scrapper.py
@@ -6,25 +6,19 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 
 path = Service("C:\\Users\\siyun\\Downloads\\chromedriver_win32\\chromedriver.exe")
-NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?'
 
-driver = webdriver.Chrome(service=path)
-driver.get(NFL_url)
+for year in range(1987, 2024):
+    
+    NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?year={yrs}&pos=&college='.format(yrs=year)
 
-headers = driver.find_elements(By.TAG_NAME, "thead")
-for element in headers:
-    text = element.text
+    driver = webdriver.Chrome(service=path)
+    driver.get(NFL_url)
 
-col_names = text.split("\n")
-col_names = col_names[:-1] + col_names[-1].split(' ')
-col_names.remove('Wonderlic')
-print(col_names)
+    data = driver.find_elements(By.TAG_NAME, "tbody")
+    for element in data:
+        text = element.text
 
-data = driver.find_elements(By.TAG_NAME, "tbody")
-for element in data:
-    text = element.text
-
-all_values = text.split("\n")
-print(all_values)
+    all_values = text.split("\n")
+    print(all_values)
 
 driver.quit()
---
+++ Src/scrape_headers.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+path = Service("C:\\Users\\siyun\\Downloads\\chromedriver_win32\\chromedriver.exe")
+
+NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?'
+
+driver = webdriver.Chrome(service=path)
+driver.get(NFL_url)
+
+headers = driver.find_elements(By.TAG_NAME, "thead")
+for element in headers:
+    text = element.text
+
+col_names = text.split("\n")
+col_names = col_names[:-1] + col_names[-1].split(' ')
+col_names.remove('Wonderlic')
+print(col_names)
+
+driver.quit()
