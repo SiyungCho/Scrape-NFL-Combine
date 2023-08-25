a0f6746fd3b97fe6c5588c8406c267379b0745b6
Siyung Cho <20fsc@queensu.ca>
2023-08-25 04:25:42 -0400

    reformat

--- all_players_scrapper.py
+++ all_players_scrapper.py
@@ -1,19 +1,20 @@
-#C:\Users\siyun\Downloads\WebScrappingBets\.venv\Scripts\python.exe
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 path = Service("C:\\Users\\siyun\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
-all_players = list()
+data_table = []
 for year in range(1987, 2024):
     
     NFL_url = 'https://nflcombineresults.com/nflcombinedata.php?year={yrs}&pos=&college='.format(yrs=year)
     driver = webdriver.Chrome(service=path)
     driver.get(NFL_url)
 
-    data = driver.find_elements(By.TAG_NAME, "tbody")
-    for element in data:
-        text = element.text
+    table = driver.find_element(By.ID, 'datatable')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
 
-    all_players.append(text.split("\n"))
+    for row in rows[1:]:  # Skip the header row
+        cols = row.find_elements(By.TAG_NAME, 'td')
+        player_data = [col.text if col.text.strip() != '' else '0' for col in cols]
+        data_table.append(player_data)
     driver.quit()
--- all_pro_scrapper.py
+++ all_pro_scrapper.py
@@ -3,16 +3,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 path = Service("C:\\Users\\siyun\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
-all_pro_players = list()
+all_pro_players = []
 for year in range(1987, 2023):
 
     NFL_url ='https://www.pro-football-reference.com/years/{yrs}/probowl.htm'.format(yrs=year)
     driver = webdriver.Chrome(service=path)
     driver.get(NFL_url)
 
-    data = driver.find_elements(By.TAG_NAME, "tbody")
-    for element in data:
-        text = element.text
+    table = driver.find_element(By.ID, 'datatable')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
 
-    all_pro_players.append(text.split("\n"))
+    for row in rows[1:]:  # Skip the header row
+        cols = row.find_elements(By.TAG_NAME, 'td')
+        player_data = [col.text if col.text.strip() != '' else '0' for col in cols]
+        all_pro_players.append(player_data)
     driver.quit()
