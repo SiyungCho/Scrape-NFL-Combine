4e648fb713647cc83a8cb7536acd9c27f4be9ac2
SiyungCho <siyungcho@gmail.com>
2023-09-08 17:20:25 -0400

    delete unused files

--- player.py
+++
@@ -1,6 +0,0 @@
-class player:
-    def __init__(self, name, school_name, year, stats):
-        self.name = name
-        self.school_name = school_name
-        self.year = year
-        self.stats = stats
